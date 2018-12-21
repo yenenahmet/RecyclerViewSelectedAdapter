5e2a773fbe3cc5aa4ddcae3c4b44eb34f47f4443
yenenahmet <yenenahmet@hotmail.com>
2018-12-21 19:10:09 +0300

    Update RecyclerViewBaseSelectedAdapter.java

--- recyclerviewselectedadapter/src/main/java/com/example/recyclerviewselectedadapter/RecyclerViewBaseSelectedAdapter.java
+++ recyclerviewselectedadapter/src/main/java/com/example/recyclerviewselectedadapter/RecyclerViewBaseSelectedAdapter.java
@@ -1,38 +1,36 @@
 package com.example.ahmet.benimbutcem.MyLib.RecyclerViewHelper;
 
 import android.support.v7.widget.RecyclerView;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 /**
  * Created by Ahmet on 26.11.2017.
  */
 public abstract class RecyclerViewBaseSelectedAdapter<T, E extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<E> {
 
     private List<T> items;
-    private List<Integer> SelectedList;
+    private List<Integer> selectedList;
 
     protected RecyclerViewBaseSelectedAdapter() {
-        this.items = new ArrayList<T>();
-        this.SelectedList = new ArrayList<>();
+        this.selectedList = new ArrayList<>();
     }
 
     @Override
     public int getItemCount() {
-        return  items==null ? 0: items.size();
+        return  this.items==null ? 0: this.items.size();
     }
 
     public  void setItems(List<T> items) {
-        if (items != null && !items.isEmpty()) {
+        if (items != null) {
+            clearItems();
             this.items = items;
             notifyDataSetChanged();
         }
     }
 
     public void setChanged(int position ,T item){
-        if(item !=null && position>-1 && !this.items.isEmpty() ){
+        if(item !=null && position>-1 &&this.items!= null &&!this.items.isEmpty() ){
             this.items.set(position,item);
             notifyItemChanged(position,item);
         }
@@ -42,11 +40,11 @@ public abstract class RecyclerViewBaseSelectedAdapter<T, E extends RecyclerView.
     }
 
     public  T getItem(int position) {
-        return this.items.get(position);
+      return this.items.get(position);
     }
 
     public  void addItem(T item) {
-        if (item != null) {
+        if (item != null && this.items !=null) {
             this.items.add(item);
             final int position =this.items.size() - 1;
             notifyItemInserted(position);
@@ -54,7 +52,7 @@ public abstract class RecyclerViewBaseSelectedAdapter<T, E extends RecyclerView.
     }
 
     public  void addItems(List<T> items) {
-        if (items != null && !items.isEmpty()) {
+        if (items != null && !items.isEmpty() && this.items !=null) {
             final int x = this.items.size()-1;
             this.items.addAll(items);
             final int itemcount = items.size() -1;
@@ -63,50 +61,50 @@ public abstract class RecyclerViewBaseSelectedAdapter<T, E extends RecyclerView.
     }
 
     private void removeItem(int position) {
-        if(position>-1){
+        if(position>-1 && this.items !=null && !this.items.isEmpty()){
             this.items.remove(position);
             notifyItemRemoved(position);
         }
     }
 
     // Selected //
-    public  List<T> RemoveSelected() {
+    public  List<T> removeSelected() {
         List<T> tmp = new ArrayList<>();
-        for (Integer value : this.SelectedList) {
+        for (Integer value : this.selectedList) {
             tmp.add(getItem(value));
         }
-        Collections.sort(SelectedList);
-        for (int i = this.SelectedList.size() - 1; i >= 0; i--) {
-            removeItem(SelectedList.get(i));
+        Collections.sort(selectedList);
+        for (int i = this.selectedList.size() - 1; i >= 0; i--) {
+            removeItem(selectedList.get(i));
         }
-        this.SelectedList.clear();
+        this.selectedList.clear();
         return tmp;
     }
 
-    public void ClearSelectedList() {
-        this.SelectedList.clear();
+    public void clearSelectedList() {
+        this.selectedList.clear();
         notifyDataSetChanged();
     }
 
-    public int SelectedItem(int position) {
-        final int values = SelectedKontrol(position);
+    public int selectedItem(int position) {
+        final int values = selectedControl(position);
         if (values == -1) {
-            this.SelectedList.add(position);
+            this.selectedList.add(position);
         } else {
-            this.SelectedList.remove(values);
+            this.selectedList.remove(values);
         }
         notifyItemChanged(position);
-        return this.SelectedList.size();
+        return this.selectedList.size();
     }
 
-    public boolean SingleSelectedAgain(int positon) {
-        return positon>-1 &&!this.SelectedList.isEmpty() && this.SelectedList.get(0) == positon && this.SelectedList.size() == 1;
+    public boolean singleSelectedAgain(int positon) {
+        return positon>-1 &&!this.selectedList.isEmpty() && this.selectedList.get(0) == positon && this.selectedList.size() == 1;
     }
 
-    protected int SelectedKontrol(int position) {
+    protected int selectedControl(int position) {
         if(position>-1){
-            for (int i = 0; i < this.SelectedList.size(); i++) {
-                if (this.SelectedList.get(i) == position) {
+            for (int i = 0; i < this.selectedList.size(); i++) {
+                if (this.selectedList.get(i) == position) {
                     return i;
                 }
             }
@@ -115,18 +113,21 @@ public abstract class RecyclerViewBaseSelectedAdapter<T, E extends RecyclerView.
     }
 
     public int getSelectedItemSize() {
-        return this.SelectedList.size();
+        return this.selectedList.size();
     }
 
     protected void close(){
-        if(items != null){
-            items.clear();
-            items = null;
-        }
-        if(SelectedList !=null){
-            SelectedList.clear();
-            SelectedList = null;
+        clearItems();
+        if(this.selectedList !=null){
+            this.selectedList.clear();
+            this.selectedList = null;
         }
     }
 
+    private void clearItems(){
+        if(this.items != null){
+            this.items.clear();
+            this.items = null;
+        }
+    }
 }
